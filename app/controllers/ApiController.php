56d35d99a048f4ed37eaf49d0931f7f7d1041075
Johan Jatko <johan.jatko@outlook.com>
2014-07-04 08:22:05 +0200

    Added namechanged and ipchanged events

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -6,7 +6,7 @@ class ApiController extends \Munition\AppController {
         $server = Server::find_by_guid($server_guid)->obj();
         if($server != null) {
             if($server->auth_key == $server_key) {
-                $player_id = $this->process_player();
+                $player_id = $this->process_player($server->id);
                 $this->process_event($server->id, $player_id);
 
                 self::render(["json" => ["status" => "OK"]]);
@@ -19,7 +19,7 @@ class ApiController extends \Munition\AppController {
     }
 
 
-    private function process_player() {
+    private function process_player($server_id) {
         $player_guid = $_POST["player_guid"];
         $player_ip = $_POST["player_ip"];
         $player_name = $_POST["player_name"];
@@ -31,13 +31,17 @@ class ApiController extends \Munition\AppController {
             $id = $player->id;
             if($player->latest_username != $player_name) {
                 PlayerUsername::create(["player_id" => $id, "username" => $player_name, "changed" => $now]);
+                $tmp = $player->latest_username;
                 $player->latest_username = $player_name;
                 $player->save();
+                Event::create_with_data($id, $server_id, 'namechanged', ["before" => $tmp, "after" => $player_name]);
             }
             if($player->latest_ip != $player_ip) {
                 PlayerIp::create(["player_id" => $id, "ip" => $player_ip, "changed" => $now]);
+                $tmp = $player->latest_ip;
                 $player->latest_ip = $player_ip;
                 $player->save();
+                Event::create_with_data($id, $server_id, 'ipchanged', ["before" => $tmp, "after" => $player_ip]);
             }
             return $id;
         } else {
@@ -55,12 +59,6 @@ class ApiController extends \Munition\AppController {
         $event_type = $_POST["event_type"];
         $event_data = isset($_POST["event_data"]) ? $_POST["event_data"] : [];
 
-        $now = date("Y-m-d H:i:s");
-        $event = Event::create(["server_id" => $server_id, "player_id" => $player_id, "type" => $event_type, "submitted" => $now]);
-        $event_id = Event::get()->last->id;
-
-        foreach($event_data as $key=>$value) {
-            EventData::create(["event_id" => $event_id, "key" => $key, "value" => $value]);
-        }
+        Event::create_with_data($player_id, $server_id, $event_type, $event_data);
     }
 }
--- app/models/Event.php
+++ app/models/Event.php
@@ -1,5 +1,16 @@
 <?php
 class Event extends \DbModel\Base {
+
+    public static function create_with_data($player_id, $server_id,$event_type, $data) {
+        $now = date("Y-m-d H:i:s");
+        Event::create(["server_id" => $server_id, "player_id" => $player_id, "type" => $event_type, "submitted" => $now]);
+        $event_id = Event::get()->last->id;
+
+        foreach($data as $key=>$value) {
+            EventData::create(["event_id" => $event_id, "key" => $key, "value" => $value]);
+        }
+    }
+
     function relations() {
         $this->hasMany("eventData", ["class" => "EventData"]);
         $this->belongsTo("player");
